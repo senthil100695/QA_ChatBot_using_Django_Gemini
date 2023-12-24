2c55268a4d02edff9b68aa1882c1ae01570d6c20
senthil100695 <senthil100695@gmail.com>
2023-12-24 10:45:41 +0530

    update

--- QABot_Gemini/LLMChat/admin.py
+++ QABot_Gemini/LLMChat/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import GeminiBot
 
 # Register your models here.
+admin.site.register(GeminiBot)
--- QABot_Gemini/LLMChat/models.py
+++ QABot_Gemini/LLMChat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class GeminiBot(models.Model):
+    question = models.CharField(max_length = 255)
+    response = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.response 
